95018b9ddcb662216e9243a302a878e54601296e
Kieran McGuinness <zumaht@gmail.com>
2022-08-01 00:37:06 -0400

    added comments and fixed api display

--- handlers.js
+++ handlers.js
@@ -1,19 +1,24 @@
 const fetch = require("node-fetch");
-const url = "https://api.openweathermap.org/data/3.0/onecall";
-const appid = "2cc19a4838e6719d7f49f521a974aadb";
+const url = "https://api.openweathermap.org/data/3.0/onecall"; //API url
+const appid = process.env.API_KEY || ""; //api key goes here
 
+// handler for the initial page without a lat/lon
 const initialResponse = (req, res) => {
   res.send(
     "Add a latitude and longitude to the address bar to receive area weather. example: localhost:3000/30/94"
   );
+  console.log(
+    "Add a latitude and longitude to address bar and curl address to receive area weather. example: curl localhost:3000/30/94"
+  );
 };
 
+// handler for the request to the Open Weather API, requires a lat and lon coordinate
 const apiRequest = async (req, res) => {
-  const { lat, lon } = req.params;
-  const response = await callApi(lat, lon);
+  const { lat, lon } = req.params; //grabs the lat and lon coordinates from the request url
+  const response = await callApi(lat, lon); // calls the callApi function with the lat and lon coordinates
 
-  const data = await response.json();
-  const { temp, weather } = data.current;
+  const data = await response.json(); //converts json response
+  const { temp, weather } = data.current; // exptracts display information from the api response
   const { alerts } = data;
   let climate = "";
 
@@ -26,6 +31,7 @@ const apiRequest = async (req, res) => {
   }
 
   const display = {
+    //puts the desired information together to be returned
     condition: weather[0].main,
     climate: climate,
     alerts: alerts
@@ -33,13 +39,15 @@ const apiRequest = async (req, res) => {
       : [{ event: "no alerts in this area", description: "no alerts" }],
   };
 
-  res.send(display);
+  res.send(display); //send the desired information
+  console.log(display);
 };
 
+//function that sends the coordinates to the Open Weather API and returns the response back to the apiRequest handler
 async function callApi(lat, lon) {
   const response = await fetch(
     url +
-      `?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily&units=imperial&appid=${appid}`
+      `?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily&units=imperial&appid=${appid}` //api url with some exclusions
   );
   return response;
 }
--- readme.md
+++ readme.md
@@ -1,23 +1,38 @@
-Weather Service
+# Weather Service
 
-An http server that takes in latitude/longitude coordinates and returns weather data from the Open Weather API for the given location. The weather data that is returned includes,
-the current weather conditions outside in the area,
-temperature as either hot (80°F+), moderate(79°F-56°F), or cold (55°F-),
-and any weather alerts in the area and their description.
+## Description
 
-Technologies
-Express 4.18.1
-node-fetch 2
-Jest 28.1.3
-SuperTest 6.2.4
+An http server that takes in latitude/longitude coordinates and returns weather data from the Open Weather API for the given location. The weather data that is returned includes:
 
-Launch
+1. The current weather conditions outside in the area.
+2. The temperature outside as either hot (80°F+), moderate(79°F-56°F), or cold (55°F-).
+3. Any weather alerts in the area and their description.
+
+## Technologies
+
+- Express 4.18.1
+- node-fetch 2
+- Jest 28.1.3
+- SuperTest 6.2.4
+
+## Launch
+
+Requires an Open Weather One Call API 3.0 key. The key must be added to the appid variable in the handlers.js file. A key can be obtained here https://openweathermap.org/api
+
 To run this project locally using npm:
+
+```
 $ npm install
 $ npm start
+```
+
 The format for inputting the latitude and longitude coordinates is
-http://localhost:3000/lat/lon
-Example: http://localhost:3000/33/-94
+
+- http://localhost:3000/lat/lon
+  - Example: http://localhost:3000/33/-94
 
 The unit tests can be run with
+
+```
 $ npm test
+```
